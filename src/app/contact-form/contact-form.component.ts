fff0f12ec5f852ca794581d79b0f5368cea8d6c6
thamarais16 <thamarais16@gmail.com>
2021-08-10 14:30:38 +0530

    2 files modified

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  public firstName: string = "Thamarai";
   constructor() { }
 
   ngOnInit() {
