7362a6128d0625a3037283c59d4c980df32b6025
thamarais16 <thamarais16@gmail.com>
2021-08-23 19:49:35 +0530

    1 file modified, 1 added

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
-
+  public country: Country[] = [
+    new Country('1', 'Great Britain'),
+    new Country('2', 'India'),
+    new Country('3', 'USA'),
+    new Country('4', 'England')
+  ];
   constructor() { }
 
   ngOnInit() {
   } 
 
+  onSubmit(form){
+    console.log(form.value);
+  }
+
+}
+
+export class Country{
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string){
+    this.id = id;
+    this.name = name;
+  }
 }
